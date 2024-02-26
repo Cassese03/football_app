cd771514aa04a82139ca66eac35745092cfea593
Cassese03 <casseselorenzo03@gmail.com>
2024-02-26 14:17:44 +0100

    PROVA notifica

--- lib/common/appbar.dart
+++ lib/common/appbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:football_app/constants.dart';
 import 'package:football_app/screens/main_screen.dart';
 import 'package:iconsax/iconsax.dart';
@@ -52,7 +53,51 @@ class AppbarAll extends StatelessWidget implements PreferredSizeWidget {
       ),
       actions: [
         IconButton(
-          onPressed: () {},
+          onPressed: () {
+            //   showDialog(
+            //     context: context,
+            //     builder: (context) => Stack(
+            //       children: [
+            //         Positioned(
+            //           width: 50,
+            //           child: Column(
+            //             children: [
+            //               const Card(
+            //                 color: kprimaryColor,
+            //                 child: Text('Notifica'),
+            //               ),
+            //               const Card(
+            //                 color: kprimaryColor,
+            //                 child: Text('Notifica'),
+            //               ),
+            //               const Card(
+            //                 color: kprimaryColor,
+            //                 child: Text('Notifica'),
+            //               ),
+            //               const Card(
+            //                 color: kprimaryColor,
+            //                 child: Text('Notifica'),
+            //               ),
+            //               const Card(
+            //                 color: kprimaryColor,
+            //                 child: Text('Notifica'),
+            //               ),
+            //               ElevatedButton(
+            //                 onPressed: () => Navigator.pop(context),
+            //                 child: const Text(
+            //                   'Chiudi',
+            //                 ),
+            //               )
+            //             ],
+            //           ),
+            //         ),
+            //       ],
+            //     ),
+            //   ).then((valueFromDialog) {
+            //     // use the value as you wish
+            //     print(valueFromDialog);
+            //   });
+          },
           icon: const Icon(Iconsax.notification),
         ),
       ],
