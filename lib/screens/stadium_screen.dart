b9a675508757ab2da9ae39426731d0679a5e407a
Cassese03 <90678655+Cassese03@users.noreply.github.com>
2024-02-27 18:14:01 +0100

    Screen

--- lib/screens/stadium_screen.dart
+++ lib/screens/stadium_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:football_app/common/appbar.dart';
 import 'package:football_app/providers/players_provider.dart';
 import 'package:football_app/utils/json_handler.dart';
@@ -31,6 +34,15 @@ class _StadiumScreenState extends State<StadiumScreen> {
     } catch (e) {
       print(e);
     }
+  }
+
+  takescrshot() async {
+    RenderRepaintBoundary boundary = screenShotKey.currentContext
+        ?.findRenderObject() as RenderRepaintBoundary;
+    var image = await boundary.toImage();
+    var byteData = await image.toByteData(format: ImageByteFormat.png);
+    var pngBytes = byteData?.buffer.asUint8List();
+    return pngBytes;
   }
 
   @override
@@ -67,8 +79,28 @@ class _StadiumScreenState extends State<StadiumScreen> {
         onColorChange: (color) {
           Provider.of<PlayersProvider>(context, listen: false).setColor(color);
         },
-        onSave: () {
-          print("object");
+        onSave: () async {
+          await takescrshot().then(
+            (value) => showDialog(
+              context: context,
+              builder: (Builder) => AlertDialog(
+                title: const Column(
+                  children: [
+                    Text(
+                      textAlign: TextAlign.center,
+                      'Complimenti la tua Raimon scende in campo!',
+                    ),
+                    Text(
+                      'Dai il meglio di te in campo!',
+                    ),
+                  ],
+                ),
+                content: Image.memory(
+                  value,
+                ),
+              ),
+            ),
+          );
         },
       ),
     );
